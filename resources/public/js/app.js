2df201cb98c024bada26ca38618750378c7b8aa2
Unnikrishnan <krish8664@gmail.com>
2016-07-06 02:04:25 +0530

    Refactors reactjs

--- resources/public/js/app.js
+++ resources/public/js/app.js
@@ -1,163 +1,127 @@
-var CurrentVotes = React.createClass({
+
+var LunchSelector = React.createClass({
 	getInitialState: function(){
-		return {votes: []};
+		return({
+			currentVotes: [],
+			popularRestaurants: [],
+			restaurantList: []
+		});
 	},
-	loadVotesFromServer: function() {
-    $.ajax({
-      url: this.props.url,
+	getCurrentVotes: function(){
+		$.ajax({
+      url: "/api/current-votes",
       dataType: 'json',
       cache: false,
       success: function(data) {
-        this.setState({votes: data});
+        this.setState({currentVotes: data});
       }.bind(this),
       error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
+        console.error("/api/current-votes", status, err.toString());
       }.bind(this)
     });
-  },
-  componentDidMount: function() {
-    this.loadVotesFromServer();
-  },
-
-  render: function(){
-    var votes = this.state.votes.map(function(vote){
-      return(<RestaurantListItem name={vote.name} id={vote.rest_id} votes={vote.votes} />)});
-    return (
-	<div className="currentVotesList">
-	<h3> Current Votes: </h3>
-	<div className="list-group">
-	{votes}
-      </div>
-	</div>)
-  }
-
+	},
+	getPopularRestaurants: function(){
+		$.ajax({
+      url: "/api/popular-restaurant",
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+				this.setState({popularRestaurants: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+				console.error("/api/popular-restaurant", status, err.toString());
+      }.bind(this)
+    });
+	},
+	getRestaurantList: function(){
+		$.ajax({
+      url: "/api/restaurants",
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({restaurantList: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error("/api/restaurants", status, err.toString());
+      }.bind(this)
+    });
+	},
+	handleSubmitVotes: function(){
+		this.getCurrentVotes();
+		this.getPopularRestaurants();
+	},
+	componentDidMount: function(){
+		this.getCurrentVotes();
+		this.getPopularRestaurants();
+		this.getRestaurantList();
+	},
+	render: function(){
+		return(
+			<div className="container">
+				<div className="row">
+					<div className="col-md-6">
+						<RestaurantList data={this.state.currentVotes} type="Current Votes" onSubmitHandle={this.handleSubmitVotes} />
+					</div>
+					<div className="col-md-6">
+						<RestaurantList data={this.state.popularRestaurants} type="Popular Restaurants" onSubmitHandle={this.handleSubmitVotes} />
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-md-6">
+						<RestaurantList data={this.state.restaurantList} type="Restaurant List" onSubmitHandle={this.handleSubmitVotes} />
+					</div>
+				</div>
+			</div>
+		);
+	}
 });
 
-var RestaurantListItem = React.createClass({
-  submitvote: function(){
-    var payload = {"votes": this.props.id};
-    $.ajax({
+var RestaurantList = React.createClass({
+	getDefaultProps: function() {
+    return {
+      data: []
+    };
+  },
+	submitVote: function(e){
+		console.log(e.target.id);
+		var payload = {"votes": parseInt(e.target.id)};
+		$.ajax({
       type: "POST",
-      url: '/api/vote',
+      url: "/api/vote",
       dataType: 'json',
       contentType: 'application/json;charset=utf-8',
       data: JSON.stringify(payload),
       cache: false,
       success: function(data) {
-        this.setState({voted: true});
+        this.props.onSubmitHandle();
       }.bind(this),
       error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
+        console.error("/api/vote", status, err.toString());
       }.bind(this)
     });
-  },
-  render: function(){
-    return(<button onClick={this.submitvote} type="button " className="list-group-item">
-	   <span className="badge">{this.props.votes}</span>
-	   {this.props.name}
-	   </button>);
-  }
+	},
+	dayOfTheWeek: function(){
+		return moment.format("dddd");
+	},
+	render: function(){
+		var listItem = this.props.data.map((restaurant) => {
+			return(
+				<button type="button" className="list-group-item"  id={restaurant.rest_id} onClick={this.submitVote}>
+					<span className="badge">{restaurant.votes}</span>
+					{restaurant.name}
+				</button>
+			);
+		});
+
+		return(
+			<div className={this.props.type}>
+				<h3>{this.props.type}</h3>
+				<div className="list-group">
+					{listItem}
+				</div>
+			</div>
+		);
+	}
 });
 
-
-var RestaurantList = React.createClass({
-  loadCommentsFromServer: function() {
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      cache: false,
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  },
-
-  getInitialState: function() {
-    return {data: []};
-  },
-
-  componentDidMount: function() {
-    this.loadCommentsFromServer();
-    setInterval(this.loadCommentsFromServer, 20000);
-  },
-
-  render: function(){
-    var restaurantListNodes = this.state.data.map(function(restaurant){
-      return(
-	  <RestaurantListItem name={restaurant.name} id={restaurant.rest_id} />)
-    });
-
-    return (
-	<div className="restaurantList">
-	<h3> Restaurants: </h3>
-	<div className="list-group">
-	{restaurantListNodes}
-      </div>
-
-      </div>)
-  }
-
-});
-
-var PopularRestaurant = React.createClass({
-  loadRestaurantsFromServer: function() {
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      cache: false,
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  },
-
-  getInitialState: function() {
-    return {data: []};
-  },
-
-  componentDidMount: function() {
-    this.loadRestaurantsFromServer();
-
-  },
-
-  dayofweek: function(){
-  	var today = moment();
-  	var day = today.format("dddd");
-  	return day;
-  },
-
-
-  render: function(){
-
-    var voted_restaurants = this.state.data.map(function(vote){
-
-      return (<RestaurantListItem name={vote.name}  /> )})
-    return(
-        <div className="popularResaturant">
-        <h3> Popular restaurants for {this.dayofweek()}s : </h3>
-	<div className="list-group" >
-          {voted_restaurants}
-        </div>
-      </div>
-    )
-  }
-});
-
-
-ReactDOM.render(
-    <CurrentVotes url="/api/current-votes" />,
-  document.getElementById('current-votes'));
-
-ReactDOM.render(
-    <RestaurantList url="/api/restaurants" />,
-  document.getElementById('content'));
-
-ReactDOM.render(
-    <PopularRestaurant url="/api/popular-restaurant" />, document.getElementById('popular')
-)
+ReactDOM.render(<LunchSelector />, document.getElementById('app'));
